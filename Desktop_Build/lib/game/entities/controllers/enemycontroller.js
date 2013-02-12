82a3797901d0c72e7668c6b26c618690a033e4dd
Chris Jennison <cjennison@hitpointstudios.com>
2013-02-11 22:11:17 -0500

    Saw Blades

--- Desktop_Build/lib/game/entities/controllers/enemycontroller.js
+++ Desktop_Build/lib/game/entities/controllers/enemycontroller.js
@@ -13,17 +13,25 @@ EntityEnemyController = ig.Entity.extend({
 		xPos:null,
 		yPos:null,
 		
+		sawBladeyTimer:null,
+		
 		init:function(x,y,settings){
 			this.parent(x,y,settings);
 			
+			this.sawBladeyTimer = new ig.Timer();
 			
 			this.xPos = ig.system.width;
 			
-			ig.game.spawnEntity(EntitySawBladey, this.xPos, 100);
 		},
 		
 		update:function(){
 			this.parent();
+			
+			if(this.sawBladeyTimer.delta() > 2){
+				var yPos = Math.random()*ig.system.height - 20;
+				ig.game.spawnEntity(EntitySawBladey, this.xPos, yPos);
+				this.sawBladeyTimer.reset();
+			}
 		}
 
 });
