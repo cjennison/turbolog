d0c827f159c46e277ec1149f888d7773b6c7e0d3
Christopher Jennison <cjennison92@gmail.com>
2013-08-08 13:36:25 -0700

    Design Document

---
+++ iOS_Build/App/lib/game/screen/end-level.js
@@ -0,0 +1,24 @@
+ig.module(
+	'game.screen.end-level'
+)
+.requires(
+	'plusplus.core.config',
+    'plusplus.core.loader',
+    'plusplus.core.game',
+	
+	'impact.font',
+	'plugins.touch-button',
+	'plugins.button',
+	'game.screen.main-menu',
+	'game.screen.my-log'
+)
+.defines(function(){
+	_c = ig.CONFIG;
+	buttons = [];
+	
+	endlevel = ig.GameExtended.extend({
+		
+		
+	})
+	
+})
